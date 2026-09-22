b232d1129752ab71863983714bd1789436554bfa
erfan aghaee <erfan4erfan6@gmail.com>
2026-09-22 14:34:16 +0330

    feature: added add_product function

--- Store_managing.py
+++ Store_managing.py
@@ -1,17 +1,29 @@
 #Store_managing
 # سیستمی برای مدیریت محصولات و مشتری ها و سفارش های فروشگاه
-import mysql.connector
+import sqlite3
 import csv
+
+connection = sqlite3.connect("Store.db")
+cursor = connection.cursor()
+
 class Product:
-    def __init__(self, ID , Name , Description , Price):
-        self.id = ID
+    def __init__(self, Name , Description , Price):
         self.name = Name
         self.desc = Description
         self.price = Price
 class Customer:
-    def __init__(self , ID , Name , Password , Money):
-        self.id = ID
+    def __init__(self , Name , Password , Money):
         self.name = Name
         self.password = Password
         self.money = Money
-  
+
+def add_product(product):
+    # adding product into product table
+    try:
+        query = "INSERT INTO product(Name , Description , Price) VALUES (? , ? , ?)"
+        parametrs = (product.name , product.desc , product.price)
+        cursor.execute(query , parametrs)
+        connection.commit()
+        print("product added succesfully.")
+    except Exception as e:
+        print("error:" , e)
