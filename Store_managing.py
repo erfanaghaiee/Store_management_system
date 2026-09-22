28dc5cb517e3f5d78ef07d9abe370b3253b0b9a7
erfan aghaee <erfan4erfan6@gmail.com>
2026-09-22 09:16:30 +0330

    feature: added product and customer class

--- Store_managing.py
+++ Store_managing.py
@@ -2,3 +2,16 @@
 # سیستمی برای مدیریت محصولات و مشتری ها و سفارش های فروشگاه
 import mysql.connector
 import csv
+class Product:
+    def __init__(self, ID , Name , Description , Price):
+        self.id = ID
+        self.name = Name
+        self.desc = Description
+        self.price = Price
+class Customer:
+    def __init__(self , ID , Name , Password , Money):
+        self.id = ID
+        self.name = Name
+        self.password = Password
+        self.money = Money
+  
